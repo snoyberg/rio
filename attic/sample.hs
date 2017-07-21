0216a920371a06c6c1e23f0f904ad4e3703944ba
Michael Snoyman <michael@snoyman.com>
2017-07-21 06:09:29 +0300

    Silly sample

---
+++ attic/sample.hs
@@ -0,0 +1,18 @@
+{-# OPTIONS_GHC -F -pgmF deadlift #-}
+module Main where
+
+main :: IO ()
+main = do
+  bs :: ByteString <- B.readFile "attic/sample.hs"
+  let text = decodeUtf8 bs
+      m = Map.fromListWith (+) $ map (, 1 :: Int) (T.unpack text)
+  mapM_ sayShow $ Map.toList m
+  final <- forEach (0 :: Int)
+    (\total x -> do
+        if x <= 10
+          then do
+            say $ T.pack $ "New number is " <> show x
+            return $ Continue $! total + x
+          else return $ Break total)
+    (FE.enumFromTo 1 1000000)
+  sayShow final
