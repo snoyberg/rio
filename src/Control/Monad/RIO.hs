36b29f095ba33d90e7f0527f7eb4d9b728b8e6dd
Michael Snoyman <michael@snoyman.com>
2017-07-21 06:56:20 +0300

    MonadWriter/MonadState/etc

--- src/Control/Monad/RIO.hs
+++ src/Control/Monad/RIO.hs
@@ -1,8 +1,10 @@
 {-# LANGUAGE FlexibleInstances          #-}
+{-# LANGUAGE FunctionalDependencies     #-}
 {-# LANGUAGE GeneralizedNewtypeDeriving #-}
 {-# LANGUAGE MultiParamTypeClasses      #-}
 {-# LANGUAGE TypeFamilies               #-}
 {-# LANGUAGE TypeSynonymInstances       #-}
+{-# LANGUAGE UndecidableInstances       #-}
 -- | Defines the 'RIO' monad and some associated functions.
 --
 -- @since 0.1.0.0
@@ -11,13 +13,18 @@ module Control.Monad.RIO
   , runRIO
   , LogFunc
   , HasLogFunc (..)
+  , HasStateRef (..)
+  , HasWriterRef (..)
   ) where
 
 import Control.Monad.Base
 import Control.Monad.Catch
+import Control.Monad.Except
 import Control.Monad.Logger
 import Control.Monad.Reader
+import Control.Monad.RWS.Class
 import Control.Monad.Trans.Control
+import Data.Monoid
 import UnliftIO
 
 -- | The Reader+IO monad. This is different from a 'ReaderT' because:
@@ -36,6 +43,12 @@ import UnliftIO
 newtype RIO env a = RIO { unRIO :: ReaderT env IO a }
   deriving (Functor,Applicative,Monad,MonadIO,MonadReader env,MonadThrow,MonadCatch,MonadMask,MonadBase IO)
 
+-- | Run a 'RIO' action with the given environment.
+--
+-- Note that, for convenience of use, we pass the environment first,
+-- unlike similar functions like @runReaderT@.
+--
+-- @since 0.1.0.0
 runRIO :: MonadIO m => env -> RIO env a -> m a
 runRIO env (RIO (ReaderT f)) = liftIO (f env)
 
@@ -63,3 +76,57 @@ instance MonadBaseControl IO (RIO env) where
   type StM (RIO env) a = a
   liftBaseWith f = RIO $ ReaderT $ \r -> f $ runRIO r
   restoreM = return
+
+-- | An environment that contains a mutable reference for holding state.
+--
+-- @since 0.1.0.0
+class HasStateRef s env | env -> s where
+  getStateRef :: env -> IORef s -- TODO consider allowing something besides IORef
+instance HasStateRef s (IORef s) where
+  getStateRef = id
+
+instance HasStateRef s env => MonadState s (RIO env) where
+  put x = do
+    ref <- asks getStateRef
+    writeIORef ref $! x
+  get = asks getStateRef >>= readIORef
+
+-- | An environment that contains a mutable reference for a writer value.
+--
+-- @since 0.1.0.0
+class Monoid w => HasWriterRef w env | env -> w where
+  getWriterRef :: env -> IORef w -- TODO consider allowing something besides IORef
+  setWriterRef :: IORef w -> env -> env
+instance Monoid w => HasWriterRef w (IORef w) where
+  getWriterRef = id
+  setWriterRef ref _ = ref
+
+instance HasWriterRef w env => MonadWriter w (RIO env) where
+  writer (a, w) = do
+    ref <- asks getWriterRef
+    modifyIORef' ref (<> w)
+    return a
+
+  tell w = do
+    ref <- asks getWriterRef
+    modifyIORef' ref (<> w)
+
+  listen f = do
+    env <- ask
+    ref <- newIORef mempty
+    let env' = setWriterRef ref env
+    a <- runRIO env' f
+    w <- readIORef ref
+    return (a, w)
+
+  pass f = do
+    (a, g) <- f
+    ref <- asks getWriterRef
+    modifyIORef' ref g
+    return a
+
+instance (HasWriterRef w env, HasStateRef s env) => MonadRWS env w s (RIO env) where
+
+instance MonadError SomeException (RIO env) where -- Would be much nicer to have Exception e =>
+  throwError = throwIO
+  catchError = UnliftIO.catch
